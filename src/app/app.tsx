41ef45da1295c45d3901e641030936d8815e77a4
Thais Mai <thai2k4>
2024-10-20 21:56:39 +0700

    feat(staff): add dashboard

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,11 +5,28 @@ import Login from "./login";
 import Pond_Construction from "./pond-construction";
 import Contact from "./contact";
 import Register from "./register";
+import Dashboard from "app/dashboard/dashboard.tsx";
+import FormServiceProgress from "app/dashboard/forms/service-progress.tsx";
+import TableServiceProgress from "app/dashboard/tables/service-progress.tsx";
+import LayoutAdmin from "components/layout/dashboard";
 
 const routes = [
   { path: "/", element: <Home /> },
   { path: "/contact", element: <Contact /> },
   { path: "/pond-construction", element: <Pond_Construction /> },
+];
+
+const routesAdmin = [
+  { path: "/admin", element: <Dashboard /> },
+  { path: "/admin/dashboard", element: <Dashboard /> },
+  {
+    path: "/admin/forms/form-service-progress",
+    element: <FormServiceProgress />,
+  },
+  {
+    path: "/admin/tables/table-service-progress",
+    element: <TableServiceProgress />,
+  },
 ];
 
 function App() {
@@ -21,6 +38,14 @@ function App() {
         ))}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        {routesAdmin.map(({ path, element }) => (
+          <Route
+            key={path}
+            path={path}
+            element={<LayoutAdmin>{element}</LayoutAdmin>}
+          />
+        ))}
       </Routes>
     </Router>
   );
